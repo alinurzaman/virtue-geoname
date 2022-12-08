8f002af0d919d6dbf9384ef9efcfcfbb25bea550
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2022-12-08 12:31:05 +0700

    test again

--- demo/src/main/java/com/example/controllers/GeonameController.java
+++ demo/src/main/java/com/example/controllers/GeonameController.java
@@ -40,7 +40,7 @@ public class GeonameController {
                 return null;
             list = geonameService.findGeonameByNameLatitudeLongitude(name, lat);
         }
-        // list
+        // list projection
         List<GeonameProjection> listProjection = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             String country = "";
